ebce35d0c133a0c2aa8c187c5283211bf0e63518
Nikolay <nselivanov@ugcs.com>
2021-03-12 16:49:10 +0300

    Create test

--- tests/GetPostServiceTests.java
+++ tests/GetPostServiceTests.java
@@ -1,10 +1,23 @@
+import com.kekchpek.mytestwebservice.GetPostService;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.channels.SelectableChannel;
 
 public class GetPostServiceTests {
 
     @Test
-    public void test() {
-        
+    public void testStartup1() throws IOException, NoSuchFieldException, IllegalAccessException {
+        SelectableChannel mockChannel = Mockito.mock(SelectableChannel.class);
+        GetPostService service = GetPostService.createNew(mockChannel);
+        service.startup();
+        Field field = GetPostService.class.getDeclaredField("selector");
+        field.setAccessible(true);
+        Object selector = field.get(service);
+        Assert.assertNotNull(selector);
     }
 
 }
