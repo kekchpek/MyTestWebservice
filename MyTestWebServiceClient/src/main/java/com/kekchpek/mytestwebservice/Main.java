42f02432cfa44af7eaaba5d63fdf6c2015b06477
Nikolay <rfka1@inbox.ru>
2021-03-17 10:47:40 +0300

    Add some client impl

--- MyTestWebServiceClient/src/main/java/com/kekchpek/mytestwebservice/Main.java
+++ MyTestWebServiceClient/src/main/java/com/kekchpek/mytestwebservice/Main.java
@@ -3,12 +3,13 @@ package com.kekchpek.mytestwebservice;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        if (args == null || args.length == 1) {
-            System.out.println("Server address should be specified");
+        if (args == null || args.length == 0) {
+            System.out.println("Server address should be specified as argument");
             return;
         }
         String[] addressParts = args[0].split(":");
@@ -27,11 +28,41 @@ public class Main {
         }
         try {
             channel.connect(address);
+            channel.configureBlocking(false);
         } catch (IOException e) {
             System.out.printf("Fail to connect to the server on %s%n", args[0]);
             e.printStackTrace();
+            return;
         }
         MyTestWebServiceClient client = MyTestWebServiceClient.createNew(channel);
+        try {
+            client.startup();
+        } catch (IOException e) {
+            System.out.println("Fail to start client");
+            e.printStackTrace();
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("exit"))
+                return;
+            String[] commandParts = command.split(" ");
+            if (commandParts.length > 0) {
+                if (commandParts[0].equals("get")) {
+                    client.get(System.out::println);
+                }
+                if (commandParts[0].equals("post")) {
+                    if (commandParts.length < 2) {
+                        System.out.println("post command should has an argument");
+                        return;
+                    }
+                    client.post(commandParts[1], System.out::println);
+                }
+            } else {
+                System.out.println("Fail to execute command");
+            }
+        }
     }
 
 }
--- MyTestWebServiceClient/src/main/java/com/kekchpek/mytestwebservice/MyTestWebServiceClient.java
+++ MyTestWebServiceClient/src/main/java/com/kekchpek/mytestwebservice/MyTestWebServiceClient.java
@@ -3,14 +3,33 @@ package com.kekchpek.mytestwebservice;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channel;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.WritableByteChannel;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 public class MyTestWebServiceClient {
 
+    public interface AnswerListener {
+        void run(String answer);
+    }
+
+    private static final String GET_COMMAND = "get";
+    private static final String POST_COMMAND = "post";
+
     private final SelectableChannel channel;
     private final ByteBuffer channelBuffer;
+    private Selector selector;
+
+    private final Object answerListenerLock = new Object();
+    private AnswerListener answerListener;
 
     public static MyTestWebServiceClient createNew(SelectableChannel channel) {
         return new MyTestWebServiceClient(channel);
@@ -19,17 +38,80 @@ public class MyTestWebServiceClient {
     private MyTestWebServiceClient(SelectableChannel channel) {
         this.channel = channel;
         channelBuffer = ByteBuffer.allocate(256);
-
     }
 
-    public void post(String value) {
-
+    public void startup() throws IOException {
+        selector = Selector.open();
+        channel.register(selector, SelectionKey.OP_CONNECT);
+        ExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.submit(this::mainLoop);
     }
 
-    public String get(String value) {
-
+    public void post(String value, AnswerListener listener) {
+        if (!setAnswerListener(listener)) {
+            listener.run("Client does not support several commands executing");
+            return;
+        }
+        writeToChannel(channel, String.format("%s %s", POST_COMMAND, value));
     }
 
+    public void get(AnswerListener listener) {
+        if (!setAnswerListener(listener)) {
+            listener.run("Client does not support several commands executing");
+            return;
+        }
+        writeToChannel(channel, "get");
+    }
+
+    private Set<SelectionKey> selectKeys() {
+        if (selector == null)
+            return new HashSet<>();
+
+        try {
+            selector.select();
+            return selector.selectedKeys();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HashSet<>();
+        }
+    }
+
+    private boolean setAnswerListener(AnswerListener listener) {
+        synchronized (answerListenerLock) {
+            if (answerListener != null)
+                return false;
+            answerListener = listener;
+            return true;
+        }
+    }
+
+    private void gotMessage(String message) {
+        synchronized (answerListenerLock) {
+            if (answerListener != null)
+                answerListener.run(message);
+            answerListener = null;
+        }
+    }
+
+    private void mainLoop() {
+        while (true) {
+            Set<SelectionKey> selectionKeys = selectKeys();
+            for (SelectionKey key :
+                    selectionKeys) {
+                if (key.isReadable()) {
+                    String message = readFromChannel(key.channel());
+                    gotMessage(message);
+                }
+                if (key.isConnectable()) {
+                    try {
+                        channel.register(selector, SelectionKey.OP_READ);
+                    } catch (ClosedChannelException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
     private boolean writeToChannel(Channel c, String message) {
         if (c instanceof WritableByteChannel) {
